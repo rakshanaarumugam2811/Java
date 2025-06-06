82531075fe95c9097a8a3c75750feeae3cd45af5
rakshanaarumugam2811 <rakshanaarumugam080@gmail.com>
2025-06-06 15:22:00 +0530

    Add files via upload

---
+++ Day2/Task2.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+	              String s1="Hello";
+	              String s2=new String("Hello");
+	              String s3=s1;
+	              System.out.println(s1.equals((s2)));
+	              System.out.println(s1.equals(s3));
+                  System.out.println(s1.charAt(1));
+	}
+
+}
