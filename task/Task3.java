afdb8c30b3118382165b12074730297139ae7658
rakshanaarumugam2811 <rakshanaarumugam080@gmail.com>
2025-06-09 11:36:06 +0530

    Add files via upload

---
+++ task/Task3.java
@@ -0,0 +1,23 @@
+package task;
+
+import java.util.Scanner;
+
+public class Task3 {
+     public static void main(String[] args) {
+
+       Scanner in = new Scanner(System.in);
+
+         System.out.print("Enter a sentence: ");
+
+         String sentence = in.nextLine();
+
+         String[] words = sentence.split(" ");
+
+         System.out.println("Words in the sentence:");
+
+         for (String word : words) {
+
+             System.out.println(word);
+         }
+     }
+}
